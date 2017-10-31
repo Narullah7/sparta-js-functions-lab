9ed8c7684166e8957e8a5b7331d7c9204fcdaf3c
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-10-31 13:56:57 +0000

    did work in class

---
+++ function-starter-code/js/index.js
@@ -0,0 +1,40 @@
+
+
+function addTwo(n1 , n2) {
+  return n1 + n2
+}
+
+function addThree(n1, n2, n3) {
+  return n1 + n2 + n3;
+}
+
+function multiplyTwo(n1,n2) {
+  return n1 * n2;
+}
+
+function increaseByPercentage(total, percentage) {
+  return total + (total * (percentage/100));
+}
+
+function reverseWord(word) {
+  var reverse = word.split("").reverse().join("");
+  return reverse;
+}
+
+function celciusToFahrenheit(celcius) {
+  var CtF = celcius * (9/5) +32;
+  return CtF;
+}
+
+function fahrenheitToCelcius(fahr) {
+  var FtC = (fahr -32) * (5/9);
+  return FtC;
+}
+
+// BONUS QUESTION
+function stripVowels(word) {
+  var answer = word.replace(/[aeiou]/ig, '');
+  return answer;
+}
+
+runTests();
